b860d2fb44c9f6d868944774e48ad77e1af7d92e
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-06-11 02:03:53 +0530

    Updated the home page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,42 +2,39 @@ import Loader from 'react-loaders'
 import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Contact = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-      return setTimeout(() => {
-        setLetterClass('text-animate-hover')
-      }, 3000)
-    }, [])
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
   return (
     <>
       <div className="container contact-page">
-        <div className="text-box">  
-          <h1><br/>
-          <AnimatedLetters
-            
+        <div className="text-box">
+          <h1>
+            <AnimatedLetters
               letterClass={letterClass}
-              strArray={['C', 'o', 'n', 't', 'a', 'c', 't']}
+              strArray={['G', 'e', 't', '  ', 'i', 'n', '  ', 't', 'o', 'u', 'c', 'h']}
               idx={15}
             />
           </h1>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br>
-           Maecenas sodales pellentesque libero id consequat. <br></br>
-           In vel magna dignissim, lobortis turpis in, pretium urna.<br></br>
-            Sed blandit ac lorem vitae blandit. A
-          </p>
         </div>
-        <div className="contact-links">
-          <h2>Phone </h2>
-          
-          <h2>Mail </h2>
-          <h2>Linkedin </h2>
-          <h2>Github </h2>
-          <h2>Instagram </h2>
+        <p>
+          <br />Let's work on some exciting Machine Learning or <br />Artificial Intelligence Projects or something exciting
+        </p>
+
+        <div>
+          <a href="mailto:prathamagrawal1205@gmail.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faEnvelope} color="white"> </FontAwesomeIcon>
+          </a>
         </div>
       </div>
       <Loader type="pacman" />
