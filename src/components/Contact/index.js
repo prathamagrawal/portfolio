73e94e218e49c6da8f632fa53bbfd6e233f744f8
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-06-11 13:47:17 +0530

    Updated Home Image and About Page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,11 +3,22 @@ import './index.scss'
 import { useEffect, useState } from 'react'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub,faLinkedin, faKaggle, faInstagram} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope,faPhone} from '@fortawesome/free-solid-svg-icons'
+import { Grid } from '@mui/material'
+import {Paper} from '@mui/material'
+import {Container} from '@mui/material'
+
+
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+
+  const gridStyles = {
+    backgroundColor: "black",
+    alignItems: "center",
+
+  };
 
   useEffect(() => {
     return setTimeout(() => {
@@ -31,11 +42,57 @@ const Contact = () => {
           <br />Let's work on some exciting Machine Learning or <br />Artificial Intelligence Projects or something exciting
         </p>
 
-        <div>
-          <a href="mailto:prathamagrawal1205@gmail.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faEnvelope} color="white"> </FontAwesomeIcon>
-          </a>
+        <div className="container contact-grid">
+          <Container>
+          <Grid container rowSpacing={20} columnSpacing={{ xs: 1, sm: 2, md: 3 }} sx={gridStyles}> 
+            <Grid spacing-12 item xs={12} sm={8} md={4} >
+            <Paper sx={{backgroundColor:"black"}}>
+              <a href="mailto:prathamagrawal1205@gmail.com" target="_blank" rel="noreferrer">
+                    <FontAwesomeIcon icon={faEnvelope} color="white" size="6x"></FontAwesomeIcon>
+              </a>
+            </Paper>
+            </Grid>
+            <Grid item xs={12} sm={8} md={4} >
+           <Paper sx={{backgroundColor:"black"}}>
+              <a href="https:www.github.com/prathamagrawal" target="_blank" rel="noreferrer">
+                      <FontAwesomeIcon icon={faGithub} color="white" size="6x"></FontAwesomeIcon>
+              </a>
+            </Paper>
+            </Grid>
+            <Grid item xs={12} sm={8} md={4}>
+           <Paper sx={{backgroundColor:"black"}}>
+              <a href="https:www.linkedin.com/pratham-manish-agrawal" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faLinkedin} color="white" size="6x"></FontAwesomeIcon>
+                </a>
+            </Paper>
+            </Grid>
+
+            <Grid item xs={12} sm={8} md={4} >
+           <Paper sx={{backgroundColor:"black"}}>             
+               <a href="https:www.instagram.com/prathammanishagrawal" target="_blank" rel="noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} color="white" size="6x"></FontAwesomeIcon>
+                </a>
+            </Paper>
+            </Grid>
+            <Grid item xs={12} sm={8} md={4} >
+           <Paper sx={{backgroundColor:"black"}}>
+              <a href="https:www.kaggle.com/fearsomejockey" target="_blank" rel="noreferrer">
+                          <FontAwesomeIcon icon={faKaggle} color="white" size="6x"></FontAwesomeIcon>
+              </a>
+            </Paper>
+            </Grid>
+            <Grid item xs={12} sm={8} md={4}>
+            <Paper sx={{backgroundColor:"black"}}>
+              <a href="tel:+918269067007" target="_blank" rel="noreferrer">
+                          <FontAwesomeIcon icon={faPhone} color="white" size="6x"></FontAwesomeIcon>
+              </a>
+            </Paper>
+            </Grid>
+            </Grid>
+          </Container>
         </div>
+
+
       </div>
       <Loader type="pacman" />
     </>
