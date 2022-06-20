bfa1fdbe16bbf4f2c5434f3e8fd65d4b5195f1a6
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-06-21 00:29:23 +0530

    Projects Page and Education Page

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,30 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
+        company: 'Little Love',
+        jobtitle: 'Human Resources Director',
         startYear: '2018',
-        endYear: '2019'
+        endYear: 'Present'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
+        company: 'Institution of Engineering and Technology, VIT',
+        jobtitle: 'Chaiperson',
+        startYear: '2022',
+        endYear: 'Present'
     },
     {
         id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
+        company: 'Vellore Insitute of Technology',
+        jobtitle: 'Research',
+        startYear: '2022',
+        endYear: 'Present'
+    },
+    {
+        id: 3,
+        company: 'Klatach Technologies',
+        jobtitle: 'Data Science/ Annotation Intern',
+        startYear: '2022',
         endYear: 'Present'
     },
 ]
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,6 +1,6 @@
 import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
+import two from '../assets/svg/projects/two.png'
+import three from '../assets/svg/projects/three.png'
 import four from '../assets/svg/projects/four.svg'
 import five from '../assets/svg/projects/five.svg'
 import six from '../assets/svg/projects/six.svg'
@@ -11,39 +11,39 @@ import eight from '../assets/svg/projects/eight.svg'
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Employee Management',
-        projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'Olympics Dataset and Visualization',
+        projectDesc: 'Contributed a dataset consisting of 124 years of Olympics Data, and Visualization to understand trends and outliers',
+        tags: ['Python', 'R', 'Dart','Plotly'],
+        code: 'https://github.com/prathamagrawal/Analysing-Olympics-Dataset',
+        demo: 'https://www.kaggle.com/datasets/fearsomejockey/olympics-dataset-2020-tokyo-dataset',
         image: one
     },
     {
         id: 2,
-        projectName: 'Railway Tracking',
-        projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+        projectName: 'IPL Analysis',
+        projectDesc: 'Through this project, I contributed an annual dataset of Batsman and Bowler Statistics.',
+        tags: ['Jupyter','Pandas','Python'],
+        code: 'https://github.com/prathamagrawal/IPL-Analysis',
+        demo: 'https://www.kaggle.com/datasets/fearsomejockey/ipl-batting-and-bowling-dataset-20182022',
         image: two
     },
     {
-        id: 3,
-        projectName: 'Weather App',
-        projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: three
+        id: 4,
+        projectName: 'LeetCode Solutions',
+        projectDesc: 'This project involves some of the solutions I have contributed to leetcode.',
+        tags: ['Python', 'C++','Java'],
+        code: 'https://github.com/prathamagrawal/LeetCode',
+        demo: 'https://leetcode.com/prathamagrawal/',
+        image: four
     },
     {
-        id: 4,
-        projectName: 'Android Patient Tracker',
-        projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: four
+        id: 3,
+        projectName: 'HbeatR',
+        projectDesc: 'A project used to show the possibility of a person having a cardiac Disease using Machine Learning',
+        tags: ['Python','Tensorflow','Keras'],
+        code: 'https://github.com/prathamagrawal/HeartBr',
+        demo: 'https://github.com/prathamagrawal/HeartBr',
+        image: three
     },
     {
         id: 5,
