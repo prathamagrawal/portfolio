bd721c17280536076839a65142cfbb78e124ca34
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-06-29 23:27:29 +0530

    Updated Font

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -134,7 +134,7 @@ function Navbar() {
         <div className='navbar'>
             <div className='navbar--container'>
                 <h1 style={{ color: theme.secondary }}>
-                    {shortname(headerData.name)}
+                    PRATHAM
                 </h1>
 
                 <IoMenuSharp
