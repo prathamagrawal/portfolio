4a8eb5df03f4bc405394e6b88fb9dbffd2f3b521
Pratham Agrawal <58286330+prathamagrawal@users.noreply.github.com>
2022-10-06 02:39:51 +0530

    Add Research paper

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
 import resume from './assets/pdf/Resume.pdf'
 import aihealthcare from './assets/pdf/paper1.pdf'
+import ml_cvd from './assets/pdf/ML_CVD_IEEE.pdf'
 import './App.css'
+
 
 function App() {
 
@@ -26,6 +28,7 @@ function App() {
           <Route path="/blog" exact component={BlogPage} />
           <Route path="/projects" exact component={ProjectPage} />
           <Route path='/journal' component={() => { window.location.href = aihealthcare; return null;}}/>
+          <Route path='/mlpaper' component={() => { window.location.href = ml_cvd; return null;}}/>
           <Route path='/resume' component={() => { window.location.href = resume; return null;}}/>
           <Route path='/github' component={() => { window.location.href = socialsData.github; return null;}}/>
           <Route path='/linkedin' component={() => { window.location.href = socialsData.linkedIn; return null;}}/>
